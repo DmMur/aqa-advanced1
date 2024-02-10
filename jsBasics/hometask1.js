361156933c6277513b1dd0c37d29851cf70dd9e6
DmMur <dmmuryhin@gmail.com>
2024-02-11 00:25:57 +0200

    basics js examples

---
+++ jsBasics/hometask1.js
@@ -0,0 +1,14 @@
+const numberOne = 5;
+let myFood='pizza'
+console.log (numberOne, myFood);
+
+let number2=true;
+let number3=false;
+console.log (number2, number3);
+
+let number4;
+let number5 = null;
+let number6 = 5* number5;
+console.log (number4, number5, number6);
+
+
