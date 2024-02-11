33fe4e69907b9aa1920705dd7324abe3d2e31080
DmMur <dmmuryhin@gmail.com>
2024-02-11 22:58:56 +0200

    New hometask4 JS

---
+++ home4js/task1.js
@@ -0,0 +1,8 @@
+const averageGrade = 55;
+
+ if (averageGrade <60 ){console.log ('your rating is unsatisfactory')}
+ else if (averageGrade >= 60 && averageGrade <=70  ){console.log ('your rating is Satisfactory')}
+ else if (averageGrade >=70 && averageGrade <=80  ){console.log ('your rating is Good')}
+ else if (averageGrade >=80 && averageGrade <=90  ){console.log ('your rating is Very Good')}
+ else if (averageGrade >=90 && averageGrade <=100  ){console.log ('your rating is Excellent')}
+ else {console.log ('System error')}
---
+++ home4js/task3.js
@@ -0,0 +1,15 @@
+const a = 5 // for 
+for 
+(let i = 1; i<=10; ++i)
+ {
+console.log (a, '*', i,'=', a*i)
+};
+
+
+const n=5 // while
+let it = 1;
+while (it <=10) {
+    console.log (n, '*' ,it, '=', n*it )   
+
+    it++;
+};
